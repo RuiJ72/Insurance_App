b6f5e6919923df9b4f168b893ec0308ea525260a
RuiJ72 <rlagos900@gmail.com>
2022-09-02 22:18:32 -0300

    GET -> POST -> PUT and DELETE methods created. Division of responsabilities of the Api using Controllers and Models

---
+++ AulaDevWeek/src/Controllers/PessoaController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using src.Models;
+
+namespace src.Controllers
+{
+
+    [ApiController]
+    [Route("[Controller]")] // Get localhost:7165/
+    public class PessoaController : ControllerBase
+    {
+        private string id;
+
+        [HttpGet]
+        public Pessoa Get()
+        {
+            Pessoa pessoa = new Pessoa("Rui", 50, "12345678");
+            Contrato novoContrato = new Contrato("abc123", 50.63);
+            
+            pessoa.contratos.Add(novoContrato);
+            
+
+            return pessoa;
+        }
+
+        [HttpPost]
+        public Pessoa Post([FromBody]Pessoa pessoa)
+        {
+            return pessoa;
+        }
+
+        [HttpPut("{id}")]
+        public string Update([FromRoute]int id, [FromBody]Pessoa pessoa)
+        {
+            Console.WriteLine(id);
+            Console.WriteLine(pessoa);
+            return " Dados do id " + id + " atualizados ";
+        }
+
+        [HttpDelete("{id}")]
+        public string Delete([FromRoute]int id)
+        {
+            return " deletadada a pessoa com o id " + id;
+        }
+    }
+}
---
+++ AulaDevWeek/src/Models/Pessoa.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+
+
+namespace src.Models
+{
+    public class Pessoa
+    {
+
+        public Pessoa()
+        {
+            this.Nome = "template";
+            this.Idade = 0;
+            this.contratos = new List<Contrato>();
+            this.Ativo = true;
+        }
+
+        public Pessoa(string nome, int idade, string Cpf)
+        {
+            this.Nome = nome;
+            this.Idade = idade;
+            this.Cpf = Cpf;
+            this.contratos = new List<Contrato>();
+            this.Ativo = true;
+            
+        }
+
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+        public string? Cpf { get; set; }
+        public bool Ativo { get; set; }
+        public List<Contrato> contratos { get; set; }
+    }
+}
