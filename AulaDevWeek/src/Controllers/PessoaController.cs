cf3f839b844241f70b8f59abd40d3593881611ef
RuiJ72 <rlagos900@gmail.com>
2022-09-04 15:55:23 -0300

    Entity Framework configuration. CRUD completed

--- AulaDevWeek/src/Controllers/PessoaController.cs
+++ AulaDevWeek/src/Controllers/PessoaController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using src.Models;
+using Microsoft.EntityFrameworkCore;
+using src.Persistence;
 
 namespace src.Controllers
 {
@@ -7,38 +9,54 @@ namespace src.Controllers
     [ApiController]
     [Route("[Controller]")] // Get localhost:7165/
     public class PessoaController : ControllerBase
-    {
-        private string id;
+    {   
+        private DatabaseContext _context { get; set; }
+
+        public PessoaController(DatabaseContext context)
+        {
+            this._context = context;
+        }
+
+        //private string id;
 
         [HttpGet]
-        public Pessoa Get()
+        public List<Pessoa> Get()
         {
-            Pessoa pessoa = new Pessoa("Rui", 50, "12345678");
-            Contrato novoContrato = new Contrato("abc123", 50.63);
-            
-            pessoa.contratos.Add(novoContrato);
-            
+            //Pessoa pessoa = new Pessoa("Rui", 50, "12345678");
+            //Contrato novoContrato = new Contrato("abc123", 50.63);
 
-            return pessoa;
+            //pessoa.contratos.Add(novoContrato);
+
+
+           return _context.Pessoas.Include(p => p.contratos).ToList();
+            //return pessoa;
         }
 
         [HttpPost]
         public Pessoa Post([FromBody]Pessoa pessoa)
-        {
+        {   
+            _context.Pessoas.Add(pessoa);
+            _context.SaveChanges();
+            
             return pessoa;
         }
 
         [HttpPut("{id}")]
         public string Update([FromRoute]int id, [FromBody]Pessoa pessoa)
         {
-            Console.WriteLine(id);
-            Console.WriteLine(pessoa);
+            _context.Pessoas.Update(pessoa);
+            _context.SaveChanges();
+
             return " Dados do id " + id + " atualizados ";
         }
 
         [HttpDelete("{id}")]
-        public string Delete([FromRoute]int id)
+        public string Delete([FromRoute]int id, [FromBody]Pessoa pessoa)
         {
+            var result = _context.Pessoas.SingleOrDefault(e => e.Id == id);
+
+            _context.Pessoas.Remove(result);
+            _context.SaveChanges();
             return " deletadada a pessoa com o id " + id;
         }
     }
--- AulaDevWeek/src/Models/Contrato.cs
+++ AulaDevWeek/src/Models/Contrato.cs
@@ -19,9 +19,11 @@
             
         }
         
+        public int Id { get; set; }
         public DateTime DataCriacao { get; set; }
         public string TokenId { get; set; }
         public double Valor { get; set; }
         public bool Pago { get; set; }
+        public int PessoaId { get; set; }
     }
 }
